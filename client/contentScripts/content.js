bd1a7b27083cea22aaab9549bc60fe264921afcb
99robong <junoh9911@gmail.com>
2024-04-30 10:49:32 +0900

    get thumbnail link

--- client/contentScripts/content.js
+++ client/contentScripts/content.js
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 // document.documentElement.style.visibility = 'hidden';  // 초기에 전체 페이지를 숨깁니다.
 
 // const customHtmlUrl = chrome.runtime.getURL("ytblock.html");
@@ -269,24 +270,36 @@ class Video {
 // 	// chrome.storage.local.set({ 'videos': videos });
 // });
 
+=======
+>>>>>>> cccf40f (get thumbnail link)
 window.onload = function() {
 	console.log('------------------------');
 
+	// get a list of Youtube videos
 	var videos = this.document.getElementsByClassName('style-scope ytd-rich-item-renderer');
 	console.log(videos);
 
 	for (var i = 0; i < videos.length; i++) {
+		// 3 array list for each videos
+		// 1st and 2nd have title and link
+		// 3rd has any 'h3' tag, so return an error
 		var video = videos[i];
 		console.log(video);
 		try {
+			// title
 			title = video.getElementsByTagName('h3')[0].outerText;
 			console.log(i, "-------------------");
 			console.log(title);
+			// naive blind
 			if (title = '') {
 				video.style.display='none';
 			}
+			// general video link
 			link = video.getElementsByTagName('a')[0].href;
-			console.log(link);
+			id = link.split('=')[1];
+			// thumbnail link
+			thumbnail = ("https://img.youtube.com/vi/"+id+"/0.jpg");
+			console.log(thumbnail);
 		} catch (error) {
 			console.log(i, "===================");
 			console.log(error);
