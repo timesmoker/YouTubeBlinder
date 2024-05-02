2cff6221611fbd990f36f168ea11449b470d2c9f
timesmoker3526 <timesmoker3526@gmail.com>
2024-05-03 03:25:10 +0900

    받는 json 형식 변경

--- server/YouTubeBlinder/youtubeblinder.js
+++ server/YouTubeBlinder/youtubeblinder.js
@@ -28,7 +28,17 @@ const server = net.createServer((socket) => {
     //const currentConnectionId =connectionId++;
     let thisid=connectionId++;
     socket.on('data', (data) => {
-        const req = JSON.parse(data);
+        let req;
+        try {
+            req = JSON.parse(data);
+        } catch (error) {
+            if (error instanceof SyntaxError) {
+                console.error('JSON 형식오류', data);
+                return;
+            } else {
+                throw error; // re-throw the error unchanged
+            }
+        }
         //const ip = socket.remoteAddress;
 
         /*  if (req.path === '/topic') {
@@ -39,34 +49,32 @@ const server = net.createServer((socket) => {
               // Send response
               socket.write(JSON.stringify({ message: 'Topic received' }));
           } else */
-        if (req.path === '/data') {
-            console.log('Request from client', thisid);
-            const { title,URL } = req.body; //안쓰는 상수 오류 떠서 제목이랑 URL만 남김, 받아서 썸네일 이미지에서 제목 뽑을거임
-            const startTimeChatGpt = Date.now(); // 걸린시간 측정하려고
+        console.log('Request from client', thisid);
+        const { title,URL } = req; //안쓰는 상수 오류 떠서 제목이랑 URL만 남김, 받아서 썸네일 이미지에서 제목 뽑을거임
+        const startTimeChatGpt = Date.now(); // 걸린시간 측정하려고
 
-            console.log('연관어:');
-            console.log(chatgpt(title)); // 연관 주제 확인
+        console.log('연관어:');
+        console.log(chatgpt(title)); // 연관 주제 확인
 
 
-            const endTimeChatGpt = Date.now();
-            console.log('chatGPT API call 소요시간: ', endTimeChatGpt - startTimeChatGpt, 'ms');
+        const endTimeChatGpt = Date.now();
+        console.log('chatGPT API call 소요시간: ', endTimeChatGpt - startTimeChatGpt, 'ms');
 
-            const startTimeDetectText = Date.now();
-            console.log('썸네일 내 제목:');
-            console.log(detectTextFromImageUrl(URL)); // 썸네일 이미지에서 제목 확인
-            const endTimeDetectText = Date.now();
-            console.log('vision API call 소요시간: ', endTimeDetectText - startTimeDetectText, 'ms');
+        const startTimeDetectText = Date.now();
+        console.log('썸네일 내 제목:');
+        console.log(detectTextFromImageUrl(URL)); // 썸네일 이미지에서 제목 확인
+        const endTimeDetectText = Date.now();
+        console.log('vision API call 소요시간: ', endTimeDetectText - startTimeDetectText, 'ms');
 
-            let response = {};
+        let response = {};
 
-            // 30% chance to send the title back
-            if (Math.random() <= 0.3) {
-                response = { title: title };
-                console.log('Title:', title, 'banned');
-            }
-
-            socket.write(JSON.stringify(response));
+        // 30% chance to send the title back
+        if (Math.random() <= 0.3) {
+            response = { title: title };
+            console.log('Title:', title, 'banned');
         }
+
+        socket.write(JSON.stringify(response));
 
 
     });
