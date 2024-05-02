f8bdaf817afaddffb8d79de4ba4dc933d78e9d4e
99robong <junoh9911@gmail.com>
2024-05-03 03:54:05 +0900

    connect server

--- client/contentScripts/content.js
+++ client/contentScripts/content.js
@@ -275,21 +275,27 @@ class Video {
 window.onload = function() {
 	console.log('------------------------');
 
+	const ip = "54.180.135.122";
+	const port = "2018";
+	const socket = new WebSocket(`wss://${ip}:${port}`);
+	socket.onopen = function(event) {
+		console.log('Connection opened');
+	};
 	// get a list of Youtube videos
 	var videos = this.document.getElementsByClassName('style-scope ytd-rich-item-renderer');
-	console.log(videos);
+	// console.log(videos);
 
 	for (var i = 0; i < videos.length; i++) {
 		// 3 array list for each videos
 		// 1st and 2nd have title and link
 		// 3rd has any 'h3' tag, so return an error
 		var video = videos[i];
-		console.log(video);
+		// console.log(video);
 		try {
 			// title
 			title = video.getElementsByTagName('h3')[0].outerText;
 			console.log(i, "-------------------");
-			console.log(title);
+			// console.log(title);
 			// naive blind
 			if (title = '') {
 				video.style.display='none';
@@ -299,14 +305,29 @@ window.onload = function() {
 			id = link.split('=')[1];
 			// thumbnail link
 			thumbnail = ("https://img.youtube.com/vi/"+id+"/0.jpg");
-			console.log(thumbnail);
+			// console.log(thumbnail);
+			json = JSON.stringify({ title: title, URL: thumbnail });
+			console.log(json);
+			socket.current.send(json);
 		} catch (error) {
-			console.log(i, "===================");
+			// console.log(i, "===================");
 			console.log(error);
-			console.log(i, "===================");
-			console.log(video);
-			console.log(i, "===================");
+			// console.log(i, "===================");
+			// console.log(video);
+			// console.log(i, "===================");
 		}
 	}
+
+	socket.onmessage = function(event) {
+		console.log('Message from server ', event.data);
+	};
+
+	socket.onclose = function(event) {
+		console.log('Connection closed');
+	};
+
+	socket.onerror = function(error) {
+		console.log('WebSocket Error: ' + error);
+	};
 };
 >>>>>>> 144f325 (get title and link)
