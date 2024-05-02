1a283dcd2e15f6eed87e8f60db78cec8834e0f53
timesmoker3526 <timesmoker3526@gmail.com>
2024-05-03 04:16:44 +0900

    websocket으로 변경

--- server/YouTubeBlinder/youtubeblinder.js
+++ server/YouTubeBlinder/youtubeblinder.js
@@ -1,9 +1,10 @@
 
 //import collapse from "bootstrap/js/src/collapse";
+import WebSocket from 'ws';
 import { chatgpt, detectTextFromImageUrl } from '../api/api.js';
-import net from 'net';
 //const mysql = require('mysql');
 let connectionId = 0;
+const wss = new WebSocket.Server({ port: 2018 });
 /*
 const db = mysql.createConnection({
     host: 'localhost',
@@ -24,13 +25,15 @@ global.db = db;
 // Store topics by user IP
 const topics = {};
 */
-const server = net.createServer((socket) => {
-    //const currentConnectionId =connectionId++;
-    let thisid=connectionId++;
-    socket.on('data', (data) => {
+
+wss.on('connection', (ws) => {
+    console.log('Client', connectionId, ' connected');
+    let thisid = connectionId++;
+
+    ws.on('message', (data) => {
         let req;
         try {
-            req = JSON.parse(data);
+            req = JSON.parse(data.toString());
         } catch (error) {
             if (error instanceof SyntaxError) {
                 console.error('JSON 형식오류', data);
@@ -39,24 +42,13 @@ const server = net.createServer((socket) => {
                 throw error; // re-throw the error unchanged
             }
         }
-        //const ip = socket.remoteAddress;
 
-        /*  if (req.path === '/topic') {
-              const topic = req.body.topic;
-
-              topics[ip] = topic;
-
-              // Send response
-              socket.write(JSON.stringify({ message: 'Topic received' }));
-          } else */
         console.log('Request from client', thisid);
-        const { title,URL } = req; //안쓰는 상수 오류 떠서 제목이랑 URL만 남김, 받아서 썸네일 이미지에서 제목 뽑을거임
-        const startTimeChatGpt = Date.now(); // 걸린시간 측정하려고
+        const { title, URL } = req;
 
+        const startTimeChatGpt = Date.now();
         console.log('연관어:');
         console.log(chatgpt(title)); // 연관 주제 확인
-
-
         const endTimeChatGpt = Date.now();
         console.log('chatGPT API call 소요시간: ', endTimeChatGpt - startTimeChatGpt, 'ms');
 
@@ -74,16 +66,12 @@ const server = net.createServer((socket) => {
             console.log('Title:', title, 'banned');
         }
 
-        socket.write(JSON.stringify(response));
-
-
+        ws.send(JSON.stringify(response));
     });
 
-    socket.on('end', () => {
-        console.log('Client',thisid,' disconnected');
+    ws.on('close', () => {
+        console.log('Client', thisid, ' disconnected');
     });
 });
 
-server.listen(2018, () => {
-    console.log('2018포트 열려있음');
-});
+console.log('WebSocket server is running on port 2018');
