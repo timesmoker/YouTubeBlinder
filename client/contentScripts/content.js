082ac8cde8bb8faf515a55a4523d246afa4f520a
99robong <junoh9911@gmail.com>
2024-05-03 06:16:06 +0900

    server failed

--- client/contentScripts/content.js
+++ client/contentScripts/content.js
@@ -1,15 +1,15 @@
 window.onload = function() {
 	console.log('------------------------');
 
-	const ip = "54.180.135.122";
-	const port = "2018";
-	const socket = new WebSocket(`wss://${ip}:${port}`);
-	socket.onopen = function(event) {
-		console.log('Connection opened');
-	};
+	// const ip = "54.180.135.122";
+	// const port = "3000";
+	// const socket = new WebSocket(`ws://${ip}:${port}`);
+	// socket.onopen = function(event) {
+	// 	console.log('Connection opened');
+	// };
 	// get a list of Youtube videos
 	var videos = this.document.getElementsByClassName('style-scope ytd-rich-item-renderer');
-	// console.log(videos);
+	// console.log(videos.length);
 
 	for (var i = 0; i < videos.length; i++) {
 		// 3 array list for each videos
@@ -20,11 +20,12 @@ window.onload = function() {
 		try {
 			// title
 			title = video.getElementsByTagName('h3')[0].outerText;
-			console.log(i, "-------------------");
-			// console.log(title);
-			// naive blind
-			if (title = '') {
-				video.style.display='none';
+			// console.log(i, "-------------------");
+			if (title == '') {
+				// invisible
+				// video.style.display='none';
+				// blur
+				video.style.filter = "blur(5Px)";
 			}
 			// general video link
 			link = video.getElementsByTagName('a')[0].href;
@@ -34,25 +35,25 @@ window.onload = function() {
 			// console.log(thumbnail);
 			json = JSON.stringify({ title: title, URL: thumbnail });
 			console.log(json);
-			socket.current.send(json);
+			// socket.current.send(json);
 		} catch (error) {
 			// console.log(i, "===================");
-			console.log(error);
+			// console.log(error);
 			// console.log(i, "===================");
 			// console.log(video);
 			// console.log(i, "===================");
 		}
 	}
 
-	socket.onmessage = function(event) {
-		console.log('Message from server ', event.data);
-	};
+	// socket.onmessage = function(event) {
+	// 	console.log('Message from server ', event.data);
+	// };
 
-	socket.onclose = function(event) {
-		console.log('Connection closed');
-	};
+	// socket.onclose = function(event) {
+	// 	console.log('Connection closed');
+	// };
 
-	socket.onerror = function(error) {
-		console.log('WebSocket Error: ' + error);
-	};
+	// socket.onerror = function(error) {
+	// 	console.log('WebSocket Error: ' + error);
+	// };
 };
