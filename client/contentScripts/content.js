c0d3b0372904bb0cd6f971eca6e94e78c94a74be
99robong <junoh9911@gmail.com>
2024-05-09 15:39:18 +0900

    create Video class

--- client/contentScripts/content.js
+++ client/contentScripts/content.js
@@ -275,37 +275,84 @@ class Video {
 window.onload = function() {
 	console.log('------------------------');
 
-	const ip = "3.35.220.73";
+	const ip = "3.37.177.6.sslip.io";
 	const port = "2018";
 	const socket = new WebSocket(`wss://${ip}:${port}`);
 	socket.onopen = function(event) {
 		console.log('Connection opened');
 	};
 	// get a list of Youtube videos
-	var videos = this.document.getElementsByClassName('style-scope ytd-rich-item-renderer');
+	var videos = this.document.getElementsByTagName('ytd-rich-grid-media');
 	// console.log(videos.length);
 
-	setTimeout(() => videos[0].style.display = 'none', 1000);
-	setTimeout(() => videos[3].style.filter = "blur(5Px)", 1000);
+	// setTimeout(() => videos[0].style.display = 'none', 1000);
+	// setTimeout(() => videos[3].style.filter = "blur(5Px)", 1000);
+	// if (video) {
+	// 	const parent = video.parentNode;
+	// 	video.remove();
 
+	// 	const newImage = document.createElement('img');
+	// 	newImage.src = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExNGkxeDhzMXJuZGc3dHhxbGhoZ2M5ZnI5cmI3YXVjdWViM3IyOHZnaSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/3o7bu3XilJ5BOiSGic/giphy.gif'; // 이미지 URL 설정
+	// 	newImage.alt = 'New Image';
+	// 	newImage.style.width = '30%';
+	// 	newImage.style.alignContent = 'center';
+
+	// 	parent.appendChild(newImage);
+	// }
+	// console.log(getVideoTitle(videos[68]));
+	// setTimeout(() => video = videos[68], 3000);
+
+	//video[0].style.display='none';
+
+	// videos[0].getElementsByTagName('h3')[0].outerText = videos[68].getElementsByTagName('h3')[0].outerText;
+	// videos[0].getElementsByTagName('a')[0].href = videos[68].getElementsByTagName('a')[0].href;
+
+	/*
+	if (videos.length > 30) { // videos[68]이 존재하는지 확인
+        var firstVideoTitle = videos[0].querySelector('h3');
+        var firstVideoLink = videos[0].querySelector('a');
+        var targetVideoTitle = videos[30].querySelector('h3');
+        var targetVideoLink = videos[30].querySelector('a');
+
+		console.log('-------------------------');
+        if (firstVideoTitle && firstVideoLink && targetVideoTitle && targetVideoLink) {
+            // 제목 교환
+			console.log('change Title');
+            var tempTitle = firstVideoTitle.textContent;
+            firstVideoTitle.textContent = targetVideoTitle.textContent;
+            targetVideoTitle.textContent = tempTitle;
+
+            // 링크 교환
+			console.log('change Link');
+            var tempHref = firstVideoLink.href;
+            firstVideoLink.href = targetVideoLink.href;
+            targetVideoLink.href = tempHref;
+        }
+    }
+	*/
+
+	console.log(videos.length);
+	var vidArr = [];
 	for (var i = 0; i < videos.length; i++) {
 		// 3 array list for each videos
 		// 1st and 2nd have title and link
 		// 3rd has any 'h3' tag, so return an error
-		var video = videos[i];
-		// console.log(video);
+		vidArr[i] = new Video(videos[i]);
+		console.log(vidArr[i]);
 		try {
-			// title
-			title = video.getElementsByTagName('h3')[0].outerText;
-			// console.log(i, "-------------------");
+			var thumbnail = vidArr[i].getThumbnail();
+			var channel = vidArr[i].channel;
+			console.log(thumbnail);
+			console.log(channel);
+			console.log(i, "-------------------");
 			if (title == '') {
 				// invisible
 				// video.style.display='none';
 				// blur
-				video.style.filter = "blur(5Px)";
+				vidArr[i].style.filter = "blur(5Px)";
 			}
 			// general video link
-			link = video.getElementsByTagName('a')[0].href;
+			link = getVideoLink(vidArr[i]);
 			id = link.split('=')[1];
 			// thumbnail link
 			thumbnail = ("https://img.youtube.com/vi/"+id+"/0.jpg");
@@ -334,4 +381,32 @@ window.onload = function() {
 		console.log('WebSocket Error: ' + error);
 	};
 };
+<<<<<<< HEAD
 >>>>>>> 144f325 (get title and link)
+=======
+
+function getVideoTitle(video) {
+	if (video) {
+		title = video.getElementsByTagName('h3')[0].outerText;
+		return (title);
+	}
+	return ("");
+}
+
+function getVideoLink(video) {
+	link = video.getElementsByTagName('a')[0].href;
+	return (link);
+}
+
+class Video {
+	constructor(video) {
+		this.thumbnail = video.getElementsByTagName('ytd-thumbnail')[0];
+		this.thumblink = this.thumbnail.getElementsByTagName('a')[0];
+		this.channel = video.getElementsByTagName('ytd-video-meta-block')[0].parentNode.parentNode;
+
+		function getThumbnail() {
+			return (this.thumbnail);
+		}
+	}
+}
+>>>>>>> ce9778b (create Video class)
