aea39ec37807b5041a358724e508c13dd40cc8c8
qet0506 <qet0506@naver.com>
2024-05-15 21:41:31 +0900

    fix api to okt

--- server/api/def_koalanlp.py
+++ server/api/def_koalanlp.py
@@ -4,7 +4,7 @@ from koalanlp.proc import Tagger
 
 # KoalaNLP 초기화
 def initialize_koalanlp():
-    initialize(java_options="-Xmx4g", DAON="LATEST")
+    initialize(java_options="-Xmx4g", OKT="LATEST")
 
 # Tagger 객체 생성
 tagger = None
@@ -12,14 +12,23 @@ tagger = None
 def create_tagger():
     global tagger
     if tagger is None:
-        tagger = Tagger(API.DAON)
+        tagger = Tagger(API.OKT)
 
 def analyze_text(text):
     if tagger is None:
         create_tagger()
+
+    exclude_tags = {'JX', 'SF'}
+    result_list = []
+
     tagged_paragraph = tagger(text)
-    result = [sentence.singleLineString() for sentence in tagged_paragraph]
-    return result
+    for sentence in tagged_paragraph:
+        for word in sentence:
+            for morph in word:
+                if str(morph.getTag()) not in exclude_tags:
+                    result_list.append(f"{morph.getSurface()}")
+
+    return result_list
 
 # KoalaNLP 사용 종료
 def close_koalanlp():
