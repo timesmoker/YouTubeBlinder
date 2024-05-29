6fbf41c62892dfc12d1fd45efb7339fb3ac821e5
timesmoker <96424996+timesmoker@users.noreply.github.com>
2024-05-30 00:18:44 +0900

    전체적 수정, 기능 추가 /notBanned { path: '/notBanned', title: 'str', videoId: 'str' }
    /adjacency
    { path: '/adjacency', topic: 'str'
    }
    
    /adjacencyTopic
    { path: '/adjacencyTopic', topic: 'str'
    }

--- server/YouTubeBlinder/youtubeblinder.js
+++ server/YouTubeBlinder/youtubeblinder.js
@@ -16,8 +16,8 @@ const certificate = fs.readFileSync('../Key/fullchain.pem', 'utf8');
 const credentials = { key: privateKey, cert: certificate };
 
 // HTTPS 서버 생성 및 WebSocket 서버와 연동
-const httpsServer = https.createServer(credentials, app);
-const wss = new WebSocketServer({ server: httpsServer });
+const httpsServer= https.createServer(credentials, app);
+const wss= new WebSocketServer({ server: httpsServer });
 
 // HTTPS 서버 리스닝 시작
 httpsServer.listen(2018, () => {
@@ -25,10 +25,10 @@ httpsServer.listen(2018, () => {
 });
 
 let connectionId = 0;
-const apiurl = 'http://13.125.145.225:9836/simCalculate';  // API 서버 주소
+const apiURL = 'http://13.125.145.225:9836/simCalculate';  // API 서버 주소
 let topicsAll = new Map();
 
-function addTopic(topic) {
+function addTopic(topicsAll, topic) {
     if (topicsAll.has(topic)) {
         topicsAll.set(topic, topicsAll.get(topic) + 1);
     } else {
@@ -36,27 +36,20 @@ function addTopic(topic) {
     }
 }
 
-function removeTopic(topic) {
-    if (topicsAll.has(topic)) {
-        let currentCount = topicsAll.get(topic);
-        if (currentCount > 1) {
-            topicsAll.set(topic, currentCount - 1);
-        } else {
-            topicsAll.delete(topic);
-        }
+function removeTopic(topicsAll, topic) {
+    let currentCount = topicsAll.get(topic);
+    if (currentCount > 1) {
+        topicsAll.set(topic, currentCount - 1);
+    } else {
+        topicsAll.delete(topic);
     }
 }
 
 wss.on('connection', (ws) => {
     console.log('Client', connectionId, ' connected');
-    let thisid = connectionId++;
+    let thisID = connectionId++;
     let blockType = true;
     let userTopics = new Map();
-
-    // 토픽 예시 설정
-    userTopics.set('게임', 0.6);
-    userTopics.set('발라드', 0.6);
-    userTopics.set('힙합', 0.7);
 
     ws.on('message', async (data) => {
         try {
@@ -71,54 +64,115 @@ wss.on('connection', (ws) => {
                 throw new Error('Invalid JSON format');
             }
 
-            console.log('Request from client', thisid, req);
+            console.log('Request from client', thisID, req);
 
             if (!req.path) {
                 throw new Error('Missing path in request');
             }
 
-            if (req.path === '/topic/add' && !userTopics.has(req.topic)) {
-                userTopics.set(req.topic, req.threshold);
-            } else if (req.path === '/topic/all') {
-                ws.send(JSON.stringify({ topics: Array.from(userTopics.keys()) }));
-            } else if (req.path === '/topic/remove' && userTopics.has(req.topic)) {
-                userTopics.delete(req.topic);
-            } else if (req.path === '/video') {
-                const title = req.title;
-                console.log('videoId:', req.videoId);
+            switch (req.path) {
 
-                const apiRequest = {
-                    title: title,
-                    video_id: req.videoId,
-                    topic: Array.from(userTopics.keys())
-                };
-
-                const response = await axios.post(apiurl, apiRequest);
-                console.log('Server response:', response.data);
-
-                const maxSim = response.data.maxSim;
-                const totalSim = response.data.totalSim;
-
-                let banned = false;
-
-                Array.from(userTopics.keys()).forEach((topic, index) => {
-                    const threshold = userTopics.get(topic);
-                    if (blindSim(maxSim[index], totalSim[index], threshold, blockType)) {
-                        banned = true;
+                case  '/notBanned':
+                    const notBannedRequest ={
+                        topic : req.topic,
+                        title : req.title,
+                        video_id : req.video_id
                     }
-                });
+                    axios.post(apiURL, notBannedRequest);
+                    break;
 
-                if (banned) {
-                    console.log('Title:', title, 'banned');
-                    ws.send(JSON.stringify({ title: title, banned: true }));
-                } else {
-                    console.log('Title:', title, 'not banned');
-                    ws.send(JSON.stringify({ title: title, banned: false }));
-                }
-            } else if (req.path === '/blockType') {
-                blockType = req.blockType;
-            } else {
-                throw new Error('Invalid path in request');
+                case '/topic/all':
+                    for (let topic of userTopics.keys()) {
+                        removeTopic(topicsAll, topic);
+                    }
+                    for (let i = 0; i < req.topics.length; i++) {
+                        userTopics.set(req.topics[i], (req.threshold[i] / 100));
+                    }
+                    break;
+
+                case '/topic/adjacency':
+                    const adjacencyRequest = {
+                        topic: req.topic
+                    };
+                    try {
+                        const response = await axios.post(apiURL, adjacencyRequest);
+                        ws.send(JSON.stringify(response.data));
+                    } catch (error) {
+                        console.error('Failed to process adjacency request:', error);
+                        ws.send(JSON.stringify({ error: 'Failed to process adjacency request', details: error.message }));
+                    }
+                    break;
+
+                case '/topic/adjacencyTopic':
+                    const adjacencyTopicRequest = {
+                        topicsAll: topicsAll
+                    };
+                    try {
+                        const response = await axios.post(apiURL, adjacencyTopicRequest);
+                        ws.send(JSON.stringify(response.data));
+                    } catch (error) {
+                        console.error('Failed to process adjacency topic request:', error);
+                        ws.send(JSON.stringify({ error: 'Failed to process adjacency topic request', details: error.message }));
+                    }
+                    break;
+
+                case '/topic/add':
+                    if (!userTopics.has(req.topic)) {
+                        addTopic(topicsAll, req.topic);
+                    }
+                    break;
+
+                case '/topic/remove':
+                    if (userTopics.has(req.topic)) {
+                        removeTopic(topicsAll, req.topic);
+                    }
+                    break;
+
+                case '/video':
+                    const title = req.title;
+                    console.log('videoId:', req.videoId);
+
+                    const apiRequest = {
+                        title: title,
+                        video_id: req.videoId,
+                        topic: Array.from(userTopics.keys())
+                    };
+
+                    try {
+                        const response = await axios.post(apiURL, apiRequest);
+                        console.log('Server response:', response.data);
+
+                        const maxSim = response.data.maxSim;
+                        const totalSim = response.data.totalSim;
+
+                        let banned = false;
+
+                        Array.from(userTopics.keys()).forEach((topic, index) => {
+                            const threshold = userTopics.get(topic);
+                            if (blindSim(maxSim[index], totalSim[index], threshold, blockType)) {
+                                banned = true;
+                            }
+                        });
+
+                        if (banned) {
+                            console.log('Title:', title, 'banned');
+                            ws.send(JSON.stringify({ path: req.path, title: title, banned: true }));
+                        } else {
+                            console.log('Title:', title, 'not banned');
+                            ws.send(JSON.stringify({ path: req.path, title: title, banned: false }));
+                        }
+                    } catch (error) {
+                        console.error('Failed to process video request:', error);
+                        ws.send(JSON.stringify({ error: 'Failed to process video request', details: error.message }));
+                    }
+                    break;
+
+                case '/blockType':
+                    blockType = req.blockType;
+                    break;
+
+                default:
+                    throw new Error('Invalid path in request');
             }
         } catch (error) {
             console.error('Failed to process message', error.message);
@@ -130,11 +184,10 @@ wss.on('connection', (ws) => {
         }
     });
 
+
     ws.on('close', () => {
-        console.log('Client', thisid, 'disconnected');
+        console.log('Client', thisID, 'disconnected');
     });
-
-
 
     console.log('WebSocket server is running on port 2018');
 });
