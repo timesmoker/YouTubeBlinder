34ceb6def0e3df87e98182170b22dc4dd3724ab1
timesmoker <96424996+timesmoker@users.noreply.github.com>
2024-06-11 01:28:21 +0900

    null벡터 오류 해결

--- apiserver/app.py
+++ apiserver/app.py
@@ -1,3 +1,4 @@
+import numpy as np
 from flask import Flask, request, jsonify
 import json
 import def_koalanlp as nlp
@@ -46,6 +47,46 @@ def send_to_sqlite(data):
         print('Received from SQLite server:', response.decode('utf-8'))
 
 
+def categoryCheck(category_number_str):
+    category_dict = {
+        "1": ["영화", "애니메이션"],
+        "2": ["자동차"],
+        "10": ["음악"],
+        "15": ["반려동물", "동물"],
+        "17": ["스포츠"],
+        "18": ["단편 영화"],
+        "19": ["여행", "이벤트"],
+        "20": ["게임"],
+        "21": ["동영상 블로그"],
+        "22": ["인물", "블로그"],
+        "23": ["코미디"],
+        "24": ["엔터테인먼트"],
+        "25": ["뉴스", "정치"],
+        "26": ["노하우", "스타일"],
+        "27": ["교육"],
+        "28": ["과학기술"],
+        "29": ["비영리", "사회운동"],
+        "30": ["영화"],
+        "31": ["애니메", "애니메이션"],
+        "32": ["액션", "모험"],
+        "33": ["고전"],
+        "34": ["코미디"],
+        "35": ["다큐멘터리"],
+        "36": ["드라마"],
+        "37": ["가족"],
+        "38": ["외국"],
+        "39": ["공포"],
+        "40": ["SF", "판타지"],
+        "41": ["스릴러"],
+        "42": ["단편"],
+        "43": ["프로그램"],
+        "44": ["예고편"],
+        "":[""]
+    }
+
+    return category_dict.get(category_number_str, ["Unknown Category"])
+
+
 # FastText 모델 로드
 model = fasttext.load_model('files/cc.ko.300.bin')
 apikey = load_api_key('files/api_key.txt')
@@ -71,7 +112,15 @@ def receive_data():
     topics = data.get('topic', [])
     whiteList = data.get('whiteList', {})
 
+    video_id = ''
+    tags = []
+    thumbnailurl = ''
     description = ''
+    categoryID = ''
+    channelID = ''
+
+    category = []
+
 
     if 'video_id' in data:
         video_id = data.get('video_id', '')
@@ -82,6 +131,9 @@ def receive_data():
         print("categoryID:", categoryID)
         print("channelID:", channelID)
 
+    category = categoryCheck(categoryID)
+    print("카테고리 : " + str(category))
+
     start_time = time.time()
     title_keywords = nlp.analyze_text(title)
     description_keywords = nlp.analyze_text(description)
@@ -90,22 +142,35 @@ def receive_data():
     elapsed_time = end_time - start_time
     print("형태소 분리에 걸린시간:", elapsed_time, "seconds")
     print("추출된 키워드:", title_keywords)
-
     maxSimList = []
     avgSimList = []
 
-    start_time = time.time()
+    def calculate_similarity(keywords, topic_vector, max_sim, max_sim_keywords, total_sim, keyword_count):
+        if not keywords:
+            return max_sim, max_sim_keywords, total_sim, keyword_count
+        for keyword in keywords:
+            keyword_vector = model.get_sentence_vector(keyword)
+            if np.all(keyword_vector == 0):
+                continue
+            similarity = 1 - spatial.distance.cosine(keyword_vector, topic_vector)
+            if max(similarity, max_sim) == similarity:
+                max_sim_keywords = keyword
+            max_sim = max(max_sim, similarity)
+            total_sim += similarity
+            keyword_count += 1
+        return max_sim, max_sim_keywords, total_sim, keyword_count
 
     for topic in topics:
-        max_sim = 0
+        max_sim = [0, 0, 0, 0]
         total_sim = 0
-        description_max_sim = 0
         description_avg_sim = 0
         topic_vector = model.get_sentence_vector(topic)
         keyword_count = 0
         description_keywords_count = 0
 
         whitelist_keywords = whiteList.get(topic, [])
+
+        max_sim_keywords = ["", "", "", ""]
 
         if any(keyword in whitelist_keywords for keyword in title_keywords):
             print(f"Topic '{topic}' contains a whitelisted keyword in title. Setting similarity to 0.")
@@ -125,36 +190,30 @@ def receive_data():
             avgSimList.append(0)
             continue
 
-        for keyword in title_keywords:
-            keyword_vector = model.get_sentence_vector(keyword)
-            similarity = 1 - spatial.distance.cosine(keyword_vector, topic_vector)
-            max_sim = max(max_sim, similarity)
-            total_sim += similarity
-            keyword_count += 1
-
-        for keyword in tags:
-            keyword_vector = model.get_sentence_vector(keyword)
-            similarity = 1 - spatial.distance.cosine(keyword_vector, topic_vector)
-            max_sim = max(max_sim, similarity)
-            total_sim += similarity
-            keyword_count += 1
-
-        for keyword in description_keywords:
-            keyword_vector = model.get_sentence_vector(keyword)
-            similarity = 1 - spatial.distance.cosine(keyword_vector, topic_vector)
-            description_max_sim = max(description_max_sim, similarity)
-            description_avg_sim += similarity
-            description_keywords_count += 1
+        max_sim[0], max_sim_keywords[0], total_sim, keyword_count = calculate_similarity(category, topic_vector,
+                                                                                         max_sim[0],
+                                                                                         max_sim_keywords[0], total_sim,
+                                                                                         keyword_count)
+        max_sim[1], max_sim_keywords[1], total_sim, keyword_count = calculate_similarity(title_keywords, topic_vector,
+                                                                                         max_sim[1],
+                                                                                         max_sim_keywords[1], total_sim,
+                                                                                         keyword_count)
+        max_sim[2], max_sim_keywords[2], total_sim, keyword_count = calculate_similarity(tags, topic_vector, max_sim[2],
+                                                                                         max_sim_keywords[2], total_sim,
+                                                                                         keyword_count)
+        max_sim[3], max_sim_keywords[3], description_avg_sim, description_keywords_count = calculate_similarity(
+            description_keywords, topic_vector, max_sim[3], max_sim_keywords[3], description_avg_sim,
+            description_keywords_count)
 
         avg_sim = total_sim / keyword_count if keyword_count > 0 else 0
 
-        description_max_sim = description_max_sim * 0.7
-        description_avg_sim = description_avg_sim * 0.7 / description_keywords_count if description_keywords_count > 0 else 0
+        max_sim[3] = max_sim[3] * 0.7
+        description_avg_sim = max_sim[3] * 0.7 / description_keywords_count if description_keywords_count > 0 else 0
 
-        max_sim = max(max_sim, description_max_sim)
+        highest_max_sim = max(max_sim)
         avg_sim = max(avg_sim, description_avg_sim)
 
-        maxSimList.append(max_sim)
+        maxSimList.append(highest_max_sim)
         avgSimList.append(avg_sim)
 
     end_time = time.time()
@@ -168,6 +227,11 @@ def receive_data():
 
     elapsed_time = end_time - start_time
     print("유사도 측정에 걸린시간:", elapsed_time, "seconds")
+    print("카테고리 키워드 :",max_sim_keywords[0],"카테고리 유사도:", max_sim[0])
+    print("제목 키워드 :",max_sim_keywords[1],"제목 유사도:", max_sim[1])
+    print("태그 키워드 :",max_sim_keywords[2],"태그 유사도:", max_sim[2])
+    print("소개 키워드 :",max_sim_keywords[3],"소개 유사도:", max_sim[3])
+
 
     response = {
         "maxSim": maxSimList,
@@ -184,7 +248,7 @@ def receive_data():
         'tags': tags,
         'channel_id': channelID
     }
-    # send_to_sqlite(youtube_data)
+    send_to_sqlite(youtube_data)
 
     return jsonify(response)
 
@@ -209,6 +273,15 @@ def notBanned():
     title = keep_korean(title)
     topic = data.get('topic', '')
 
+
+    tags = []
+    thumbnailurl = ''
+    description = ''
+    categoryID = ''
+    channelID = ''
+
+    category = []
+
     print("제목 가공 후:", title)
 
     # 비디오 아이디 있으면 확인
@@ -220,6 +293,8 @@ def notBanned():
         print("description:", description)
         print("categoryID:", categoryID)  # 이후에 이용예정 일단 받아오는것까지 확인
         print("channelID:", channelID)
+
+    category = categoryCheck(categoryID)
 
     # 형태소 분석 수행
     start_time = time.time()
@@ -344,3 +419,4 @@ def adjacencyTopic():
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=9836)
     #app.run(host='localhost', port=5000)
+
